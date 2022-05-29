513a3f8f336971c51ce776cf6ef71f21ffbce9ff
Abidur Rahman Chowdhury <abid43iiuc@gmail.com>
2022-05-29 13:05:58 +0600

    how type script works and the code in app.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,37 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+let student: string = 'Jeff';
+let age: number = 30;
+let isSmart: boolean = false;
+let students: string[] = ['Jeff', 'Sara', 'Bob'];
+let fees: number[] = [100, 200, 300];
+
+interface Person  {
+  name: string,
+  job?: string, // optional property .. When we want to set it as optional use the ( ? ) for it
+  employed: string | boolean ,
+  age: number,
+  location?: string | number  // or number types
+}
+
+const person: Person = {
+  name: 'Bill Clinton',
+  job: 'President',
+  age: 30,
+}
+const handelAddUser = (firstName: string,age: number, address: string): void => { // void means no return value
+  const total: number = 50;
+  console.log(firstName, age, address);
+  
+
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
---
+++ src/vanila/vanila.js
@@ -0,0 +1,8 @@
+let user = 'Robert';
+user = 55;
+user = false;
+user = { id: 1, age: 25, name: 'K' };
+
+user = [1, 2, 3, 4, 5, 6, 7, 8]
+user =['rob', 'john', 'smith']
+console.log(user);
